4c2a08c2b8c7c7498b1c395df342c8a36430fcd4
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-06-02 16:05:54 -0500

    js files completed for API and HTML routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const store = require("../db/store");
+
+//GET
+router.get("/notes", (req, res) => {
+  store
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
+
+//POST
+router.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
+//DELETE
+router.delete("/notes/:id", (req, res) => {
+  store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
